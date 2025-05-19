cba54326c0b0f19e2bfa4f0db7328eb523746bc1
Kallox <d34d.mc@gmail.com>
2025-05-19 20:03:05 -0300

    feat(handlers): Load all handlers

---
+++ src/api/handlers/load_handlers.py
@@ -0,0 +1,9 @@
+# handler_loader.py
+import importlib
+import pkgutil
+import src.api.handlers as handlers
+
+
+def load_all_handlers():
+    for _, module_name, _ in pkgutil.iter_modules(handlers.__path__):
+        importlib.import_module(f"src.api.handlers.{module_name}")
