8779454b62915e0c2403d3ff72b5fbd54f43b8cf
Kallox <d34d.mc@gmail.com>
2025-05-19 21:09:16 -0300

    feat(schemas): Generate response model from yaml

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -2,7 +2,11 @@
 Schemas for API requests and responses.
 """
 
-from pydantic import BaseModel
+from typing import Any, Dict, Tuple, Type, cast
+
+from pydantic import BaseModel, create_model
+
+RESPONSE_MODELS = {}
 
 
 class SimpleMessage(BaseModel):
@@ -11,3 +15,36 @@ class SimpleMessage(BaseModel):
     """
 
     message: str
+
+
+TYPE_MAP = {
+    "str": str,
+    "int": int,
+    "float": float,
+    "bool": bool,
+    "list": list,
+    "dict": dict,
+}
+
+
+def generate_response_model(mapping_config: dict):
+    """
+    Generate a Pydantic model class dynamically using the provided mapping configuration.
+    """
+
+    for model_name, field_mapping in mapping_config.items():
+        fields: Dict[str, Tuple[Type[Any], Any]] = {}
+        for _, field in field_mapping.items():
+            name = field.get("name")
+            type_ = field.get("type")
+            required = field.get("required", False)
+
+            default_value = ... if required else None
+
+            field_type = TYPE_MAP.get(type_, str)
+
+            fields[name] = (field_type, default_value)
+
+        # Create a new Pydantic model class
+        model_class = create_model(model_name, **cast(dict, fields))
+        RESPONSE_MODELS[model_name] = model_class
